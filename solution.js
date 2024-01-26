1b7cc9f8408fce2866cb23456b6fe9cd652e6990
Alex-Valentino Georgiev <aresfrend13@gmail.com>
2024-01-26 14:30:41 +0200

    Implemented Confirm Reservation form functionality

--- solution.js
+++ solution.js
@@ -1,3 +1,19 @@
+document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
+}
+
+
+document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
+
+function showThanksPage(e) {
+    e.preventDefault();
+    changeContent('thank-you-content');
+}
+
+
 let reservation =
 {
     startDate: null,
